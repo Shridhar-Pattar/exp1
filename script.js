3fc6ff75c98f06f0b3a331f881e133d707000cff
Shridhar-Pattar <148942399+Shridhar-Pattar@users.noreply.github.com>
2024-07-28 00:07:53 +0530

    Update

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     downloadButton.addEventListener('click', () => {
         const link = document.createElement('a');
-        link.href = 'https://github.com/Shridhar-Pattar/Portfolio-website/raw/main/Resume.pdf';
+        link.href = 'https://github.com/Shridhar-Pattar/Portfolio-website/raw/main/Resume_Shridhar_Pattar.pdf';
         link.download = 'Resume.pdf';  // Filename for the downloaded file
         document.body.appendChild(link);
         link.click();
